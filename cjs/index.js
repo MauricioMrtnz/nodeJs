a48700a5e96064bc41778497ff811f349d218007
Mauricio Martinez <mauricio.m@montechelo.online>
2023-10-19 18:00:16 -0500

    Node

---
+++ cjs/index.js
@@ -0,0 +1,6 @@
+//Modo simple
+const suma = require("./sum.js");
+//Modo Objeto
+const {suma} = require("./sum.js");
+
+console.log(suma(0 , 5)); // 5
---
+++ cjs/sum.js
@@ -0,0 +1,10 @@
+function suma(a,b) {
+    return a + b;
+}
+
+//Modo objeto
+module.exports = {
+    suma: suma,
+}
+//Modo simple
+module.exports = suma;
